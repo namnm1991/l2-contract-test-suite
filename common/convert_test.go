d603c386027a12c0ca2dfe7f67bd981f63eb661b
ducquangkstn <ducquang.kstn@gmail.com>
2020-09-28 10:13:38 +0700

    fix import

--- common/convert_test.go
+++ common/convert_test.go
@@ -1,23 +1,14 @@
 package common
 
 import (
-	"crypto/rand"
-	"fmt"
+	"testing"
+
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestUint48ToBytes(t *testing.T) {
 	var x uint64 = 1
 	data := hexutil.Encode(Uint48ToBytes(x))
 	require.Equal(t, "0x000000000001", data)
-}
-
-func TestRandom(t *testing.T) {
-	for i := 0; i < 10; i++ {
-		pubKey := make([]byte, 32)
-		rand.Read(pubKey)
-		fmt.Println(hexutil.Encode(pubKey))
-	}
 }
